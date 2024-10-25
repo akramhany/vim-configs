7127a2bb748b12a0681841727646f38edd059c50
akramhany <akramhany65@gmail.com>
2024-10-25 19:19:39 +0300

    Upload all configs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
