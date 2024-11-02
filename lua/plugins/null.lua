c8f34d1f68d8ad73fa0ece066da91985bbd1e8f3
akramhany <akramhany65@gmail.com>
2024-11-03 00:48:28 +0200

    feat: remove null-ls formatting

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -5,11 +5,7 @@ return {
     local null_ls = require("null-ls")
 
     null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier.with({
-          extra_args = { "--print-width", "100" },
-        }),
-      },
+      sources = {},
     })
   end,
 }
